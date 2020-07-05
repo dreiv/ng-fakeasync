37ae8a29bb1925f9182db98fa3d10a4b86e8a24c
Andrei Voicu <dreivoicu@gmail.com>
2020-07-05 21:16:07 +0300

    used fakeasync

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,15 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  flushMicrotasks
+} from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
 import { UsersService } from '../users.service';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 class MockUserService {
-  getUsers = () => of([]);
+  getUsers = () => Promise.resolve([]);
 }
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
+  let service: UsersService;
+  let debugEl: DebugElement;
   let fixture: ComponentFixture<UsersComponent>;
 
   beforeEach(async(() => {
@@ -22,10 +31,30 @@ describe('UsersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
+
+    service = TestBed.inject(UsersService);
     fixture.detectChanges();
+
+    debugEl = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should resolve the promise and show the users list', fakeAsync(() => {
+    spyOn(service, 'getUsers').and.callFake(() =>
+      Promise.resolve([{ name: 'John Doe' }, { name: 'Jane Doe' }])
+    );
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    // Resolve all Promises
+    flushMicrotasks();
+
+    fixture.detectChanges();
+    const itemsCount = debugEl.queryAll(By.css('li')).length;
+
+    expect(itemsCount).toBe(2);
+  }));
 });
